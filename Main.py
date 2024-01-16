57299c5b0b4451f2dc4210d57e19b4d1049d7c12
Tyishen <ethant.ca@gmail.com>
2024-01-16 12:52:02 -0500

    Commit but I screwed up naming it :P

--- Main.py
+++ Main.py
@@ -1,41 +1,50 @@
 import pygame
 import sys
 
+def drawWorld():
+    pygame.draw.circle(screen, "blue", playerCoords, 10)
+    blockDisplay.blit(debugBlock, (0, 0))
+    screen.blit(pygame.transform.scale(blockDisplay, screen.get_size()), (0, 0))
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 960))
+screen = pygame.display.set_mode((900, 900))
+blockDisplay = pygame.Surface((300, 300))
 
 clock = pygame.time.Clock()
 dt = 0
 
+# Objects
 playerCoords = pygame.Vector2(int(screen.get_width() / 2), int(screen.get_height() / 2))
-
 debugBlock = pygame.image.load("pixil-frame-0.png").convert_alpha()
+
+
 
 running = True
 while running:
     
     screen.fill("white")
-    
-    pygame.draw.circle(screen, "blue", playerCoords, 10)
-    screen.blit(debugBlock, (0, 0))
+    blockDisplay.fill((255, 255, 255))
+
+    drawWorld()
 
     keyDown = pygame.key.get_pressed()
     
     if keyDown[pygame.K_w]:
-        playerCoords.y -= 300 * dt
+        playerCoords.y -= 1200 * dt
     if keyDown[pygame.K_a]:
-        playerCoords.x -= 300 * dt
+        playerCoords.x -= 1200 * dt
     if keyDown[pygame.K_s]:
-        playerCoords.y += 300 * dt
+        playerCoords.y += 1200 * dt
     if keyDown[pygame.K_d]:
-        playerCoords.x += 300 * dt
+        playerCoords.x += 1200 * dt
     
     
     
     dt = 0.001
     #clock.tick(60)/1000
     
+    screen.blit(pygame.transform.scale(blockDisplay, screen.get_size()), (0, 0))
+
     # flip() the display to put your work on screen
     pygame.display.flip()
 
@@ -46,6 +55,8 @@ while running:
     
 pygame.quit()
 sys.exit()
+
+
 
 
 def carIso(cartVect):
