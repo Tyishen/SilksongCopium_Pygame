c1a2349e06d1ea3c2f6256037e1d413a9d5fb356
Tyishen <ethant.ca@gmail.com>
2024-01-16 13:03:22 -0500

    guys i did something cool.. matthew says pixel grid aligns

--- Main.py
+++ Main.py
@@ -2,50 +2,46 @@ import pygame
 import sys
 
 def drawWorld():
-    pygame.draw.circle(screen, "blue", playerCoords, 10)
-    blockDisplay.blit(debugBlock, (0, 0))
-    screen.blit(pygame.transform.scale(blockDisplay, screen.get_size()), (0, 0))
+    gameDisplay.fill((255, 255, 255))
+
+    gameDisplay.blit(debugBlock, (0, 0))
+    pygame.draw.circle(gameDisplay, "blue", playerCoords, 10)
+
+    outputScreen.blit(pygame.transform.scale(gameDisplay, outputScreen.get_size()), (0, 0))
+    
+
 
 pygame.init()
-screen = pygame.display.set_mode((900, 900))
-blockDisplay = pygame.Surface((300, 300))
+outputScreen = pygame.display.set_mode((900, 900))
+gameDisplay = pygame.Surface((300, 300))
 
 clock = pygame.time.Clock()
 dt = 0
 
 # Objects
-playerCoords = pygame.Vector2(int(screen.get_width() / 2), int(screen.get_height() / 2))
+playerCoords = pygame.Vector2(int(gameDisplay.get_width() / 2), int(gameDisplay.get_height() / 2))
 debugBlock = pygame.image.load("pixil-frame-0.png").convert_alpha()
-
 
 
 running = True
 while running:
-    
-    screen.fill("white")
-    blockDisplay.fill((255, 255, 255))
-
-    drawWorld()
-
     keyDown = pygame.key.get_pressed()
     
     if keyDown[pygame.K_w]:
-        playerCoords.y -= 1200 * dt
+        playerCoords.y -= 300 * dt
     if keyDown[pygame.K_a]:
-        playerCoords.x -= 1200 * dt
+        playerCoords.x -= 300 * dt
     if keyDown[pygame.K_s]:
-        playerCoords.y += 1200 * dt
+        playerCoords.y += 300 * dt
     if keyDown[pygame.K_d]:
-        playerCoords.x += 1200 * dt
+        playerCoords.x += 300 * dt
     
     
     
-    dt = 0.001
-    #clock.tick(60)/1000
-    
-    screen.blit(pygame.transform.scale(blockDisplay, screen.get_size()), (0, 0))
+    dt = clock.tick(60)/1000
 
     # flip() the display to put your work on screen
+    drawWorld() 
     pygame.display.flip()
 
     for event in pygame.event.get():
@@ -55,9 +51,6 @@ while running:
     
 pygame.quit()
 sys.exit()
-
-
-
 
 def carIso(cartVect):
     
